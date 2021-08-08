16eaeaaafbebc34cfe6365b8963704aa9ddb4423
Tea-Primoris <32336957+Tea-Primoris@users.noreply.github.com>
2021-08-08 21:44:15 +0300

    Я НЕ ЗНАЮ ЧТО Я ДЕЛАЮ

--- test/check-paths.sh
+++ test/check-paths.sh
@@ -57,14 +57,14 @@ while read -r file; do
 		ASCII)
 			continue;;
 		UTF-8)
-			if diff -d "$file" <(<"$file" iconv -c -f utf8 -t iso8859-5 2>/dev/null | tr -d $'\x7F-\x9F' | iconv -c -f iso8859-5 -t utf8 2>/dev/null); then
+			if diff -d "$file" <(<"$file" iconv -c -f utf8 -t utf8 2>/dev/null | tr -d $'\x7F-\x9F'); then
 				continue
 			else
 				echo "$file contains Unicode characters outside the ISO 8859-5 character set"
 				(( broken_files = broken_files + 1 ))
 			fi;;
 		*)
-			if diff -d "$file" <(<"$file" tr -d $'\x7F-\x9F' | iconv -c -f iso8859-5 -t utf8 2>/dev/null | iconv -c -f utf8 -t iso8859-5 2>/dev/null); then
+			if diff -d "$file" <(<"$file" tr -d $'\x7F-\x9F' | iconv -c -f utf8 -t utf8 2>/dev/null); then
 				continue
 			else
 				echo "$file contains characters outside the ISO 8859-5 character set"
